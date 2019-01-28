3f5f1f07cbdf3ef736d861e6c913c47d24f51aa2
Harvey Baldock <HarveyBaldock@AD.PLATFORMGROUP.CO.UK>
2019-01-28 11:35:38 +0000

    put countdown and timer into different html and vue, now uses two seperate functions, forms, lists and divs

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 new Vue({
   el: "#reminderapp",
   data: {
-    minutes: 0,
-    reminderName: null,
-    reminderDescription: null,
-    reminderTime: 0,
-    items: [],
-    setting: 1,
-    disabled: 0,
-    timeStamps: []
+    countdownItems: [],
+    timerItems: [],
+    timerName: null,
+    countdownReminderName: null,
+    countdownReminderDescription: null,
+    countdownReminderTime: 1,
+    countdownReminderCounter: 0,
+    timerMinCounter: 0,
+    timerSecCounter: 0,
+    CountdownMinCounter: 0,
+    CountdownSecCounter: 0
   },
   methods: {
-    addReminder: function() {
+    /*addReminder: function() {
       if (this.setting == 1) {
         this.items.push({
           id: this.items.length + 1,
@@ -33,7 +36,7 @@ new Vue({
         });
         this.startTimer2(this.items.length - 1);
       }
-    },
+    },*/
 
     addCountdownReminder: function() {
       this.countdownItems.push({
@@ -42,8 +45,8 @@ new Vue({
         countdownDescription: this.countdownReminderDescription,
         countdownTime: this.countdownReminderTime,
         countdownCounter: this.countdownReminderTime * 60,
-        CountdownMinCounter: 0,
-        CountdownSecCounter: 0
+        countdownMinCounter: 0,
+        countdownSecCounter: 0
       });
       this.startCountdownTimer(this.countdownItems.length - 1);
     },
@@ -57,17 +60,20 @@ new Vue({
         timerSecCounter: 0
       });
       this.startTimerTimer(this.timerItems.length - 1);
+      console.log("add timer fired");
     },
 
-    removeTimer: function() {
+    removeTimer: function(index) {
       this.timerItems.splice(index, 1);
     },
 
-    removeCountdownReminder: function() {},
-
-    removeReminder: function(index) {
-      this.items.splice(index, 1);
+    removeCountdownReminder: function(index) {
+      this.countdownItems.splice(index, 1);
     },
+
+    /*removeReminder: function(index) {
+      this.items.splice(index, 1);
+    },*/
     startTimer: function(index) {
       setTimeout(() => {
         if (this.items[index].counter == 0) {
@@ -98,6 +104,37 @@ new Vue({
           if (this.items[index].secCounter == 60) {
             this.items[index].minCounter++;
             this.items[index].secCounter = 0;
+          }
+        }
+      }, 1000);
+    },
+    startCountdownTimer: function(index) {
+      setTimeout(() => {
+        if (this.countdownItems[index].countdownCounter == 0) {
+          alert("Timer finished");
+        } else {
+          this.startCountdownTimer(index);
+          this.countdownItems[index].countdownCounter--;
+          this.countdownItems[index].countdownSecCounter++;
+          console.log(this.countdownItems[index].countdownSecCounter);
+          if (this.countdownItems[index].countdownSecCounter == 60) {
+            this.countdownItems[index].countdownMinCounter++;
+            this.countdownItems[index].countdownSecCounter = 0;
+          }
+        }
+      }, 1000);
+    },
+    startTimerTimer: function(index) {
+      setTimeout(() => {
+        if (this.timerItems[index].timerCounter == undefined) {
+        } else {
+          this.startTimerTimer(index);
+          this.timerItems[index].timerCounter++;
+          this.timerItems[index].timerSecCounter++;
+
+          if (this.timerItems[index].timerSecCounter == 60) {
+            this.timerItems[index].timerMinCounter++;
+            this.timerItems[index].timerSecCounter = 0;
           }
         }
       }, 1000);
