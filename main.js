43806b195e90ebae9246e8a5301d7b1b48c5ed05
Harvey Baldock <HarveyBaldock@AD.PLATFORMGROUP.CO.UK>
2019-01-30 16:54:09 +0000

    remove button now checks for empty array and resets id number if empty

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 new Vue({
   el: "#reminderapp",
   data: {
+    countId: 0,
+    timeId: 0,
     countdownItems: [],
     timerItems: [],
     timerName: null,
@@ -19,7 +21,7 @@ new Vue({
   methods: {
     addCountdownReminder: function() {
       this.countdownItems.push({
-        Id: this.countdownItems.length + 1,
+        Id: this.countId + 1,
         countdownTitle: this.countdownReminderName,
         countdownDescription: this.countdownReminderDescription,
         countdownTime: this.countdownReminderTime,
@@ -27,26 +29,34 @@ new Vue({
         countdownMinCounter: 0,
         countdownSecCounter: 0
       });
+      this.countId++;
       this.startCountdownTimer(this.countdownItems.length - 1);
     },
 
     addTimer: function() {
       this.timerItems.push({
-        Id: this.timerItems.length + 1,
+        Id: this.timeId + 1,
         timerTitle: this.timerName,
         timerCounter: 0,
         timerMinCounter: 0,
         timerSecCounter: 0
       });
+      this.timeId++;
       this.startTimerTimer(this.timerItems.length - 1);
     },
 
     removeTimer: function(index) {
       this.timerItems.splice(index, 1);
+      if (this.timerItems == 0) {
+        this.timeId = 0;
+      }
     },
 
     removeCountdownReminder: function(index) {
       this.countdownItems.splice(index, 1);
+      if (this.countdownItems == 0) {
+        this.countId = 0;
+      }
     },
 
     finishedItemRemove: function(index) {
@@ -91,7 +101,9 @@ new Vue({
     timeStamp: function(index) {
       this.timeStamps.push({
         timeStampId: this.timerItems[index].Id,
-        timeStamp: this.timerItems[index].timerCounter
+        timeStamp: this.timerItems[index].timerCounter,
+        timeStampSec: this.timerItems[index].timerSecCounter,
+        timeStampMin: this.timerItems[index].timerMinCounter
       });
       console.log(this.timeStamps);
     }
